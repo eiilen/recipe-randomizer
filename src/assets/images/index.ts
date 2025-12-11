9d9bd07c8730525ea9022f3501b54b8f69c22a4b
ei <ei.saltedfish@gmail.com>
2025-12-11 10:19:06 +0700

    add images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -7,12 +7,18 @@ import hc3 from './hua_cheng_3.png';
 import hc4 from './hua_cheng_4.png';
 import hc5 from './hua_cheng_5.png';
 import hc6 from './hua_cheng_6.png';
+import hc7 from './hua_cheng_7.png';
+import hc8 from './hua_cheng_8.png';
+import hc9 from './hua_cheng_9.png';
 import xl1 from './xie_lian_1.png';
 import xl2 from './xie_lian_2.png';
 import xl3 from './xie_lian_3.png';         
 import xl4 from './xie_lian_4.png';
 import xl5 from './xie_lian_5.png';
 import xl6 from './xie_lian_6.png';
+import xl7 from './xie_lian_7.png';
+import xl8 from './xie_lian_8.png';
+import xl9 from './xie_lian_9.png';
 
 export {
     bg1,
@@ -24,10 +30,16 @@ export {
     hc4,        
     hc5,
     hc6,
+    hc7,
+    hc8,
+    hc9,
     xl1,
     xl2,
     xl3,
     xl4,
     xl5,
     xl6,
+    xl7,
+    xl8,
+    xl9,
 }
